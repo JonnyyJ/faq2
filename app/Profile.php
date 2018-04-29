e3bef91ad21c948849913d09cb8f6760bd5f6336
jonnyy <jc958@njit.edu>
2018-04-29 08:44:28 -0700

    Added answerForm and questionForm view pages;

--- app/Profile.php
+++ app/Profile.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class profile extends Model
 {
+    protected $fillable = ['fname', 'lname', 'body'];
     //
     public function user()
     {
+
         return $this->belongsTo('App\User');
     }
 }
